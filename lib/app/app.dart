db49ccd021af323f1c9813c17308ec3b64f2403e
AdityaNarayan1285 <aditya.narayan.12aug@gmail.com>
2026-06-09 22:37:42 +0530

    build : add initial dependencies

---
+++ lib/app/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'theme.dart';
+
+class AlgorithmVisualizerApp extends StatelessWidget {
+  const AlgorithmVisualizerApp({
+    super.key,
+    
+  
+  })
+}
